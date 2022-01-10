5d0fa4fa3fc7df0f7924db21f3d8dee2be4a8f38
KanG98 <kylexue@Yankangs-MBP.fios-router.home>
2022-01-10 18:24:39 -0500

    complete part 6

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function addRows(){
             let cell = addRow.insertCell(i)
         }
     }
+    updateCellIds();
 }
 //add columns to the grid
 function addColumns() {
@@ -21,14 +22,38 @@ function addColumns() {
             let cell = row[i].insertCell(0)
         }
     }
+    updateCellIds()
 }
 //remove rows from the grid
 function removeRows() {
   grid.deleteRow(0)
+  updateCellIds()
 }
 //remove columns from the grid
 function removeColumns() {
     for(let i = 0; i < row.length; i++){
         row[i].deleteCell(0)
     }
+    updateCellIds()
+}
+
+function updateCellIds(){
+    for(let i = 0; i < grid.rows.length; i++){
+        for(let j = 0; j < grid.rows[i].cells.length; j++){
+            grid.rows[i].cells[j].id = "tr" + i + '-' + j
+            grid.rows[i].cells[j].onclick = function(){
+                let r = this.parentElement.rowIndex
+                let c = this.cellIndex;
+                console.log(this.id)
+                fillColor(this.id, "red")
+            }
+        }
+    }
+}
+
+function fillColor(id, color){
+    let penColor = document.getElementById("color-select")
+    color = penColor.value
+    let cell = document.getElementById(id)
+    cell.style.background = color
 }
